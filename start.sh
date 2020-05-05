a55a954571b08068d91ec5fd5fb8d17736c29088
bharanitharanm <bharanitharan.m@gmail.com>
2020-05-05 01:27:24 -0400

    Updated start scritps

--- start.sh
+++ start.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+echo "Starting Node application..."
 # Start Node Application
-docker run mbharanitharan/prometheus-poc:latest
+docker run -p 3001:3001 -d mbharanitharan/prometheus-poc
+echo "Node Application started"
 
+echo "Starting Prometheus..."
 # Setup and Start Prometheus
-wget https://github.com/prometheus/prometheus/releases/download/v2.18.0-rc.0/prometheus-2.18.0-rc.0.linux-amd64.tar.gz
-tar -xf prometheus-2.18.0-rc.0.linux-amd64.tar.gz
-cd prometheus-2.18.0-rc.0.linux-amd64/
+wget -nc https://github.com/prometheus/prometheus/releases/download/v2.18.0-rc.0/prometheus-2.18.0-rc.0.linux-amd64.tar.gz 1> /dev/null
+tar -xf prometheus-2.18.0-rc.0.linux-amd64.tar.gz 1> /dev/null
 cp prometheus/* prometheus-2.18.0-rc.0.linux-amd64/ 
-./prometheus
+cd prometheus-2.18.0-rc.0.linux-amd64/
+./prometheus > promehteus.log 2>&1 &
+echo "Prometheus started"
 
+echo "Starting Alert Manager..."
 # Setup and Start Alert Manager
 cd ../
-wget https://github.com/prometheus/alertmanager/releases/download/v0.20.0/alertmanager-0.20.0.linux-amd64.tar.gz
-tar -xf alertmanager-0.20.0.linux-amd64.tar.gz
-cd alertmanager-0.20.0.linux-amd64/
+wget -nc https://github.com/prometheus/alertmanager/releases/download/v0.20.0/alertmanager-0.20.0.linux-amd64.tar.gz 1> /dev/null
+tar -xf alertmanager-0.20.0.linux-amd64.tar.gz 1> /dev/null
 cp alert-manager/* alertmanager-0.20.0.linux-amd64/
-./alertmanager
+cd alertmanager-0.20.0.linux-amd64/
+./alertmanager > alertmanager.log 2>&1 &
+echo "Alert Manager started"
+
