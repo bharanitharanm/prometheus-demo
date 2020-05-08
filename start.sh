c18b55dcf865799a22bab86e074447f8d6c779e2
bharanitharanm <bharanitharan.m@gmail.com>
2020-05-08 07:52:23 -0400

    Updated scripts

--- start.sh
+++ start.sh
@@ -5,17 +5,16 @@ echo "Starting Node application..."
 docker run -p 3001:3001 -d mbharanitharan/prometheus-poc
 echo "Node Application started"
 
-echo "Starting Prometheus..."
 # Setup and Start Prometheus
 echo "Downloading Prometheus..."
 wget -nc https://github.com/prometheus/prometheus/releases/download/v2.18.0-rc.0/prometheus-2.18.0-rc.0.linux-amd64.tar.gz &> /dev/null
 tar -xf prometheus-2.18.0-rc.0.linux-amd64.tar.gz 1> /dev/null
 cp prometheus/* prometheus-2.18.0-rc.0.linux-amd64/ 
 cd prometheus-2.18.0-rc.0.linux-amd64/
+echo "Starting Prometheus..."
 ./prometheus > promehteus.log 2>&1 &
 echo "Prometheus started"
 
-echo "Starting Alert Manager..."
 # Setup and Start Alert Manager
 cd ../
 echo "Downloading Alert Manager..."
@@ -23,6 +22,7 @@ wget -nc https://github.com/prometheus/alertmanager/releases/download/v0.20.0/al
 tar -xf alertmanager-0.20.0.linux-amd64.tar.gz 1> /dev/null
 cp alert-manager/* alertmanager-0.20.0.linux-amd64/
 cd alertmanager-0.20.0.linux-amd64/
+echo "Starting Alert Manager..."
 ./alertmanager > alertmanager.log 2>&1 &
 echo "Alert Manager started"
 
