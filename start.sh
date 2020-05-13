612ddc157b162ddb3464a2ea2e5cbd9793f42536
bharanitharanm <bharanitharan.m@gmail.com>
2020-05-13 02:28:20 -0400

    Removed log redirects for binary download and extraction

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ echo "Node Application started"
 
 # Setup and Start Prometheus
 echo "Downloading Prometheus..."
-wget -nc https://github.com/prometheus/prometheus/releases/download/v2.18.0-rc.0/prometheus-2.18.0-rc.0.linux-amd64.tar.gz &> /dev/null
-tar -xf prometheus-2.18.0-rc.0.linux-amd64.tar.gz 1> /dev/null
+wget -nc https://github.com/prometheus/prometheus/releases/download/v2.18.0-rc.0/prometheus-2.18.0-rc.0.linux-amd64.tar.gz
+tar -xf prometheus-2.18.0-rc.0.linux-amd64.tar.gz
 cp prometheus/* prometheus-2.18.0-rc.0.linux-amd64/ 
 cd prometheus-2.18.0-rc.0.linux-amd64/
 echo "Starting Prometheus..."
@@ -18,8 +18,8 @@ echo "Prometheus started"
 # Setup and Start Alert Manager
 cd ../
 echo "Downloading Alert Manager..."
-wget -nc https://github.com/prometheus/alertmanager/releases/download/v0.20.0/alertmanager-0.20.0.linux-amd64.tar.gz &> /dev/null
-tar -xf alertmanager-0.20.0.linux-amd64.tar.gz 1> /dev/null
+wget -nc https://github.com/prometheus/alertmanager/releases/download/v0.20.0/alertmanager-0.20.0.linux-amd64.tar.gz
+tar -xf alertmanager-0.20.0.linux-amd64.tar.gz
 cp alert-manager/* alertmanager-0.20.0.linux-amd64/
 cd alertmanager-0.20.0.linux-amd64/
 echo "Starting Alert Manager..."
